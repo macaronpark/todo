fe388d8010a50832e7db760aa49b316a332d06f7
macaronpark <pxxxsxzy.0@gmail.com>
2024-09-04 00:24:39 +0900

    jsx를 컴포넌트로 분리

---
+++ src/ui/CategorySideBar/CategorySideBar.tsx
@@ -0,0 +1,11 @@
+import styles from './CategorySideBar.module.scss';
+
+const CategorySideBar = () => {
+  return (
+    <div className={styles.wrapper}>
+      <p>lsb</p>
+    </div>
+  );
+};
+
+export default CategorySideBar;
---
+++ src/ui/index.ts
@@ -0,0 +1,3 @@
+export { default as CategorySideBar } from './CategorySideBar';
+export { default as TaskDetail } from './TaskDetail';
+export { default as TaskList } from './TaskList';
