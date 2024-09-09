862bcd85258118007c3b25106fbefd84249b9307
macaronpark <pxxxsxzy.0@gmail.com>
2024-09-10 00:54:58 +0900

    절대 경로, 경로 별칭 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   css: {
     preprocessorOptions: {
       scss: {
-        //todo: 경로 설정
         additionalData: `@import '/src/styles/index.scss';`,
       },
     },
