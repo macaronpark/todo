61f834c452ab00ca29a710a0ddd3b906917cc7f3
macaronpark <pxxxsxzy.0@gmail.com>
2024-09-12 00:15:17 +0900

    에러메시지 오타 수정

--- src/features/task/model/addTask.ts
+++ src/features/task/model/addTask.ts
@@ -14,7 +14,7 @@ const addTask = async ({
     onSuccess?.();
   } catch (error) {
     if (error instanceof Error) {
-      throw new Error(`❌ 새 카테고리를 만들 수 없습니다.: ${error.message}`);
+      throw new Error(`❌ 새 태스크를 만들 수 없습니다.: ${error.message}`);
     }
 
     throw new Error('❌ 알 수 없는 에러가 발생했습니다.');
