cd0ae391cb55a47ef0000cd668a9a538a452d98a
macaronpark <pxxxsxzy.0@gmail.com>
2024-10-01 16:24:42 +0900

    카테고리 이름 수정 컴포넌트와 기능 추가

---
+++ src/features/category-update/category-update.ui.tsx
@@ -0,0 +1,66 @@
+import { useCallback, useRef } from 'react';
+import useCategoryUpdate from './category-update.hook';
+
+type TProps = {
+  id: number;
+  title: string;
+  newTitle: string;
+  setNewTitle: React.Dispatch<React.SetStateAction<string>>;
+  onEndEditing: () => void;
+};
+
+const CategoryUpdateInput = ({
+  id,
+  title,
+  newTitle,
+  setNewTitle,
+  onEndEditing,
+}: TProps) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleInputRef = useCallback((input: HTMLInputElement) => {
+    if (!input) return;
+
+    input.focus();
+    input.select();
+    inputRef.current = input;
+  }, []);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      inputRef.current?.blur();
+    }
+  };
+
+  const { updateCategory } = useCategoryUpdate();
+  const handleInputBlur = async () => {
+    if (title === newTitle) {
+      onEndEditing();
+      return;
+    }
+
+    const newCategory = { title: newTitle, id };
+    await updateCategory({
+      newCategory,
+      onError: (error: Error) => {
+        setNewTitle(title);
+        window.alert(`❌ 카테고리 수정에 실패했습니다.: ${error.message}`);
+      },
+      onFinally: () => {
+        onEndEditing();
+      },
+    });
+  };
+
+  return (
+    <input
+      ref={handleInputRef}
+      value={newTitle}
+      onChange={(e) => setNewTitle(e.target.value)}
+      onKeyDown={handleKeyDown}
+      onBlur={handleInputBlur}
+    />
+  );
+};
+
+export default CategoryUpdateInput;
---
+++ src/features/category-update/index.ts
@@ -0,0 +1,1 @@
+export { default as CategoryUpdateInput } from './category-update.ui';
