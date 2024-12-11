cfc13a14fd4c57c24e4599410924db5887814930
macaronpark <pxxxsxzy.0@gmail.com>
2024-12-11 23:35:06 +0900

    temp

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var cors = require('cors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const client = require('./postgres');
 
-var indexRouter = require('./routes/index');
-var categoriesRouter = require('./routes/categories');
+const indexRouter = require('./routes/index');
+const categoriesRouter = require('./routes/categories');
+// const tasksRouter = require('./routes/tasks');
 
-var app = express();
+const app = express();
+client.connect;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/categories', categoriesRouter);
+// app.use('/tasks', tasksRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ server/postgres.js
@@ -0,0 +1,11 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
+});
+
+module.exports = client;
